4e9a86feeafdded8d6486c68c8c4caa13c470dba
Emmanuel Hernandez <e805177@gmail.com>
2024-12-31 01:13:08 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ import tailwind from "@astrojs/tailwind";
 
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://menus.bysmax.com',
   integrations: [mdx(), sitemap(), tailwind(), react()],
   adapter: vercel({
     webAnalytics: {
