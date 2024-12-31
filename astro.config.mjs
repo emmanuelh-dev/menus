e62cd3c32f222f28c6e2a0b8d6e2f11b432d65f0
Emmanuel Hernandez <e805177@gmail.com>
2024-12-31 01:08:24 -0600

    Add Vercel integration and structure restaurant page layout with details for Astoria La Bodega

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,15 @@ import tailwind from "@astrojs/tailwind";
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
