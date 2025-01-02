74809017a5e35172247f73dde1763bd1abe80c85
Emmanuel Hernandez <e805177@gmail.com>
2025-01-01 18:06:53 -0600

    Refactor Header and Menus layout to enhance filtering options and simplify navigation links

---
+++ src/data/header-nav-links.ts
@@ -0,0 +1,9 @@
+export const navItems = [
+    { name: "Características", href: "#" },
+    { name: "Precios", href: "/precios" },
+    { name: "Contacto", href: "https://forms.gle/Y4ZFXyS4KhsoQT5D6" },
+    {
+        name: "¿Hay un problema o tienes una sugerencia?",
+        href: "https://forms.gle/ZfzxRPGhh2BahKph6",
+    },
+];
