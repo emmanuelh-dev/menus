0a2a8cdded4c1f1f40b92166af2926bfece10426
Emmanuel Hernandez <e805177@gmail.com>
2025-01-04 23:50:04 -0600

    update header and index page

--- src/data/header-nav-links.ts
+++ src/data/header-nav-links.ts
@@ -5,7 +5,7 @@ export const navItems = [
     {name: "Generador de códigos QR", href: "https://www.bysmax.com/tools/qr-code-generator"},
     { name: "Contacto", href: "https://forms.gle/Y4ZFXyS4KhsoQT5D6" },
     {
-        name: "¿Hay un problema o tienes una sugerencia?",
+        name: "Informar un problema o sugerencia",
         href: "https://forms.gle/ZfzxRPGhh2BahKph6",
     },
 ];
