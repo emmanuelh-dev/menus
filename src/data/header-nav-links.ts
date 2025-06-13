c373e92a33415c3cc972225a1e594b4bcccb24ce
Emmanuel Hernandez <e805177@gmail.com>
2025-06-13 00:47:37 -0600

    feat: enhance Header component with dropdown menu and improve navigation structure; add navCategories for better organization

--- src/data/header-nav-links.ts
+++ src/data/header-nav-links.ts
@@ -11,3 +11,37 @@ export const navItems = [
     { name: "Contacto", href: "/contact" },
     { name: "Ingresa", href: "/admin/login" }, 
 ];
+
+export const navCategories = [
+    {
+        title: "Plataforma",
+        items: [
+            { name: "Características", href: "#" },
+            { name: "Precios", href: "/precios" },
+            { name: "Menús", href: "/menus" },
+            { name: "Blog", href: "/blog" },
+        ]
+    },
+    {
+        title: "Categorías",
+        items: [
+            { name: "Cafeterías", href: "/cafeterias" },
+            { name: "Espadas", href: "/espadas" },
+            { name: "Moteles", href: "/moteles" },
+        ]
+    },
+    {
+        title: "Herramientas",
+        items: [
+            { name: "Generador de códigos QR", href: "https://www.bysmax.com/tools/qr-code-generator" },
+        ]
+    },
+    {
+        title: "Social & Soporte",
+        items: [
+            { name: "Instagram", href: "https://www.instagram.com/larutadelostragones/" },
+            { name: "Contacto", href: "/contact" },
+            { name: "Ingresa", href: "/admin/login" },
+        ]
+    }
+];
