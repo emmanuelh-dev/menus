836d85f0b04f45794788c1997e210e14f5d9a800
Emmanuel Hernandez <e805177@gmail.com>
2026-01-15 17:39:11 -0600

    feat: agregar archivo sitemap para moteles y generar URLs dinámicamente

---
+++ src/pages/sitemap-moteles.xml.ts
@@ -0,0 +1,31 @@
+import { getStates } from '../lib/supabase';
+
+export async function GET() {
+  const baseUrl = 'https://menus.bysmax.com';
+  
+  const states = await getStates();
+  
+  const motelUrls = [
+    '/moteles',
+    '/moteles/estados',
+    ...states.map(state => `/moteles/estados/${state.slug}`)
+  ];
+  
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${motelUrls.map(url => `
+  <url>
+    <loc>${baseUrl}${url}</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${url === '/moteles' ? '1.0' : url === '/moteles/estados' ? '0.9' : '0.8'}</priority>
+  </url>`).join('')}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=86400'
+    },
+  });
+}
