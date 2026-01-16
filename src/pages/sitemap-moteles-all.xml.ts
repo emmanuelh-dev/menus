bd5509a744bcfc6de5a17d4434aec55d77658f96
Emmanuel Hernandez <e805177@gmail.com>
2026-01-15 18:55:04 -0600

    fix

---
+++ src/pages/sitemap-moteles-all.xml.ts
@@ -0,0 +1,39 @@
+import { getStates, getRestaurants } from '../lib/supabase';
+
+export async function GET() {
+  const baseUrl = 'https://menus.bysmax.com';
+  
+  const states = await getStates();
+  const moteles = await getRestaurants({ type: 'motel' });
+  
+  const motelUrls: string[] = [
+    '/moteles',
+    '/moteles/estados',
+    ...states.map(state => `/moteles/estados/${state.slug}`),
+    ...moteles
+      .filter(motel => motel.short_name && motel.state_id)
+      .map(motel => {
+        const state = states.find(s => s.id === motel.state_id);
+        return state ? `/moteles/estados/${state.slug}/${motel.short_name}` : null;
+      })
+      .filter((url): url is string => url !== null)
+  ];
+  
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${motelUrls.map(url => `
+  <url>
+    <loc>${baseUrl}${url}</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${url === '/moteles' ? '1.0' : url === '/moteles/estados' ? '0.9' : url.split('/').length === 4 ? '0.8' : '0.7'}</priority>
+  </url>`).join('')}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=86400'
+    },
+  });
+}
